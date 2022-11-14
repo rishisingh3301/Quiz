73cc509e1ed1ada21cfdfb74e3d0d8c6ec25ee76
Rishi <rishisingh3301@gmail.com>
2022-11-15 00:11:58 +0530

    question

--- home/models.py
+++ home/models.py
@@ -7,7 +7,7 @@ class registration(models.Model):
 
     name = models.CharField(primary_key=True, max_length=100)
     email = models.EmailField(max_length=254)
-    grade = models.CharField( max_length=100)
+    grade = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     about = models.TextField()
     score = models.IntegerField(default=0)
@@ -16,12 +16,12 @@ class registration(models.Model):
         return self.name + "  -->  " + self.email
 
 
-#  #   def __str__(self):
-#   #      return self.name + " - - " + self.blogcontent
+class question(models.Model):
+    qid = models.CharField(primary_key=True, max_length=100)
+    qtype = models.CharField(max_length=100)
+    qtext = models.TextField()
 
 
-# class score(models.Model):
+#  class score(models.Model):
 #     name = models.ForeignKey(registration, on_delete=models.CASCADE)
 #     score = models.IntegerField(default=0)
-    
-    
